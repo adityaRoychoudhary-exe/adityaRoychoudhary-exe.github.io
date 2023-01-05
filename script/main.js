42cd5674c6ebdf82d4c8b680c707535cc074cc1c
kujojotaroooo <107541824+adityaRoychoudhary-exe@users.noreply.github.com>
2023-01-05 20:22:45 +0530

    First Commit

---
+++ script/main.js
@@ -0,0 +1,9 @@
+//navbar
+const toggleMenu = () => document.body.classList.toggle("open");
+
+//button
+function buttonCl() {
+  alert(
+    "Contact information has been received by the Dev, will reach out shortly!"
+  );
+}
